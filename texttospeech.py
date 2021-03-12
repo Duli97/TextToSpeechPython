f951dd59a098f8007a28abafa21875803008f08c
Duli97 <dwindula@gmail.com>
2021-03-13 00:34:35 +0530

    no message

--- texttospeech.py
+++ texttospeech.py
@@ -1,31 +1,34 @@
 import azure.cognitiveservices.speech as speechsdk
 
-# Creates an instance of a speech config with specified subscription key and service region.
-# Replace with your own subscription key and service region (e.g., "westus").
-speech_key, service_region = "2a04a1e2bb0f47f0b9c90c5d56b5ebe2", "centralindia"
+# Replace with your own subscription key and region identifier
+speech_key, service_region = "60332a7e2e5d40ceb4f993a0405a9b48", "centralindia"
 speech_config = speechsdk.SpeechConfig(
     subscription=speech_key, region=service_region)
 
-# uncomment this line to change the voice used for synthesis
-# speech_config.speech_synthesis_voice_name = "en-CA-Linda"
+# Creates an audio configuration that points to an audio file.
+# Replace with your own audio filename.
+audio_filename = "e:/Python/TextToSpeechPython/text-to-speech-py.wav"
+audio_output = speechsdk.audio.AudioOutputConfig(filename=audio_filename)
 
-# Creates a speech synthesizer using the default speaker as audio output.
-speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
+# Creates a synthesizer with the given settings
+speech_synthesizer = speechsdk.SpeechSynthesizer(
+    speech_config=speech_config, audio_config=audio_output)
 
 # Receives a text from console input.
 print("Type some text that you want to speak...")
 text = input()
 
-# Synthesizes the received text to speech.
-# The synthesized speech is expected to be heard on the speaker with this line executed.
+# Synthesizes the text to speech.
 result = speech_synthesizer.speak_text_async(text).get()
 
 # Checks result.
 if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
-    print("Speech synthesized to speaker for text [{}]".format(text))
+    print("Speech synthesized to [{}] for text [{}]".format(
+        audio_filename, text))
 elif result.reason == speechsdk.ResultReason.Canceled:
     cancellation_details = result.cancellation_details
-    print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+    print("Speech synthesis canceled: {}".format(
+        cancellation_details.reason))
     if cancellation_details.reason == speechsdk.CancellationReason.Error:
         if cancellation_details.error_details:
             print("Error details: {}".format(
